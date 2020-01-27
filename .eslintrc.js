4176a3b4c01bd5fc05d5ba3c9e4617ea37030715
l5x <189198+l5x@users.noreply.github.com>
2020-01-27 14:51:01 +0400

    chore: updates
    Closes #289
    Closes #288
    Closes #287

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'eslint-disable-next-line @typescript-eslint/require-await': 0,
     '@typescript-eslint/require-await': 0,
     '@typescript-eslint/promise-function-async': 0,
+    '@typescript-eslint/restrict-template-expressions': 0,
     '@typescript-eslint/member-delimiter-style': [
       'error',
       {
